a45ed839d1039c7a6ef2ddc787f644251731cb4d
hrparkdev <hrparkdev@gmail.com>
2022-07-26 20:32:56 +0900

    add: Add Bean Scope

--- src/test/java/hello/core/singleton/order/OrderServiceTest.java
+++ src/test/java/hello/core/singleton/order/OrderServiceTest.java
@@ -1,9 +1,11 @@
-package hello.core.order;
+package hello.core.singleton.order;
 
 import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
